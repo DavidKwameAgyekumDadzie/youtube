252648c1ca1e440c853d08e59d740a21ee68677c
David Kwame Agyekum Dadzie <daviddadzie8@gmail.com>
2022-11-05 21:07:44 -0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "davselBookshop",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: " shefNaturalClinic",
+    title: "FREE HEALTH SCREENING",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+
+  {
+    id: 3,
+    img: "centralDogEmpire",
+    title: "CENTRAL DOG EMPIRE",
+    desc: "SELLING OF PUPPIES & ADULT DOGS, IMPORTED DOGS, STUD SERVICES, PET TRANSPORT SERVICES, GUARD DOG SERVICES.",
+    bg: "fcf1ed",
+  },
+
+  {
+    id: 4,
+    img: "fatBlaster",
+    title: "FAT BLASTER",
+    desc: "SLIM WITH EASE",
+    bg: "f5fafd",
+  },
+  {
+    id: 5,
+    img: "davselBookshop",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 6,
+    img: "davselBookshop",
+    title: "SPRING SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 7,
+    img: "davselBookshop",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 8,
+    img: "davselBookshop",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 9,
+    img: "davselBookshop",
+    title: "AUTUM SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 10,
+    img: "davselBookshop",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+];
